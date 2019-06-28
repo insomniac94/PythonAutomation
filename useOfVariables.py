966376746433a52686c2b0eadf3f38293517e37f
insomniac94 <insomniacnerd94@gmail.com>
2019-06-27 20:26:08 -0400

    added use of variables files

--- useOfVariables.py
+++ useOfVariables.py
@@ -2,6 +2,32 @@ import os
 '''
 Written by: Brandon Hough
 '''
-# clears the screen each time the program is ran
-# you must import os as shown on line 1
+
+'''
+clears the screen each time the program is ran
+you must import os as shown on line 1
+'''
 os.system('cls')
+
+'''
+You can store any data type into a variable
+Here is an example of integers and floating point numbers
+'''
+value_1 = 40
+value_2 = 50.5
+print("40 + 50.5 =", value_1 + value_2)
+
+'''
+you can change the value of a variable by setting to equal 
+to a new value
+'''
+value_1 = 59.5
+print("59.5 + 50.5 =", value_1 + value_2)
+
+'''
+Here are the 4 basic rules for variable naming conventions:
+  1.) It can only be one word
+  2.) It can use only letters, numbers, and the '_' character
+  3.) It can't begin with a number
+  4.) It can't contain a hypen character '-'
+'''
