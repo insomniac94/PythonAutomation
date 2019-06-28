767366e4a7c90686ec11ec50d908345fa954073c
insomniac94 <insomniacnerd94@gmail.com>
2019-06-27 20:43:35 -0400

    added casting examples

--- castingExamples.py
+++ castingExamples.py
@@ -4,3 +4,17 @@ Written by: Brandon Hough
 '''
 
 os.system('cls')
+
+# Here are the three types of casting you will use
+
+print("-" * 15, "Integer Casting", "-" * 15)
+print(7 + int('598'))
+print("")
+
+print("-" * 15, "String Casting", "-" * 15)
+print('Name: Alice' + ' | Age:' + str(' 21'))
+print("")
+
+print("-" * 15, "Floating Point Casting", "-" * 15)
+print(8.25 + float('5.7'))
+print("")
